fe4f2c03255d5a597fe9cc8fbcded2cbf44b5778
lxaw <LAWHALEN@email.sc.edu>
2022-06-21 14:30:34 -0400

    can add cards

--- epub_reader.py
+++ epub_reader.py
@@ -1,10 +1,23 @@
+# for reading epubs
 import ebooklib
-from bs4 import BeautifulSoup
-
 from ebooklib import epub
 from ebooklib.utils import debug
 
+# for parsing epubs
+from bs4 import BeautifulSoup
+# for json
+import json
+
+from anki_commands import invoke, request
+
+# delimiter for lines
 kDELIM = '。'
+# the shortest allowable size for text
+kLINE_THRESHOLD_SIZE = 10
+# name of new deck
+kDECK_NAME = 'yougishax'
+# name of card type
+kMODEL_NAME = 'Japanese Card'
 
 def chapter_to_str(chapter):
     soup = BeautifulSoup(chapter.get_body_content(),'html.parser')
@@ -19,7 +32,39 @@ items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
 lines = []
 for i in items:
     chapter_lines = chapter_to_str(i).split(kDELIM)
-    lines += chapter_lines
+    for line in chapter_lines:
+        if len(line) >= kLINE_THRESHOLD_SIZE:
+            lines.append(line)
 
-# make deck out of it!
+# make a deck if not present
 #
+# if kDECK_NAME not in invoke('deckNames'):
+# invoke('createDeck',deck=kDECK_NAME)
+
+dFields = {
+    'Front':'',
+    'Expression':'',
+    'Japanese Example Sentence':'',
+    'Japanese Definition':'',
+    'Pronunciation (Kana/Pinyin)':'',
+    'Test Word Alone?':'',
+    'Recording':'',
+    'Test final Kanji/Hanzi (combo)?':'',
+    'Stroke Order Diagram 1':'',
+    'Pronunciation (Pitch)':''
+}
+
+# add cards
+#
+for index,line in enumerate(lines):
+    # put in fields
+    dFields['Front'] = line
+
+    note = {
+        'deckName':kDECK_NAME,
+        'modelName':kMODEL_NAME,
+        'fields':dFields,
+        }
+    
+
+    invoke('addNote',note=note)
